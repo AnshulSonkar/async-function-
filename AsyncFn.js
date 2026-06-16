5cdec115efdd86e7d365650277099d56fb41264b
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-16 23:40:24 +0530

    Fetch-API

--- AsyncFn.js
+++ AsyncFn.js
@@ -18,20 +18,32 @@
 
 
 // USing Await 
-function getData() {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("Data Received");
-        }, 2000);
-    });
+// function getData() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve("Data Received");
+//         }, 2000);
+//     });
+// }
+
+// async function fetchData() {
+//     console.log("Loading...");
+
+//     let result = await getData();
+
+//     console.log(result);
+// }
+
+// fetchData();
+
+
+// Fetch API
+async function getProducts() {
+    let res = await fetch("https://dummyjson.com/products");
+
+    let data = await res.json();
+
+    console.log(data.products);
 }
 
-async function fetchData() {
-    console.log("Loading...");
-
-    let result = await getData();
-
-    console.log(result);
-}
-
-fetchData();
+getProducts();
