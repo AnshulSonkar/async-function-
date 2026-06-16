c7bbd427991b18de4c76fd3804bb5e44deb73119
AnshulSonkar <anshulsonkar2222@gmail.com>
2026-06-16 23:36:47 +0530

    commit

--- AsyncFn.js
+++ AsyncFn.js
@@ -2,6 +2,16 @@
 //     return "Hello";
 // }
 
-function myFunction() {
-    return Promise.resolve("Hello");
+// function myFunction() {
+//     return Promise.resolve("Hello");
+// }
+
+
+// Example 1
+async function greet() {
+    return "Hello Anshul";
 }
+
+greet().then((data) => {
+    console.log(data);
+});
